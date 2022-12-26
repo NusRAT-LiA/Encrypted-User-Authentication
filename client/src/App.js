02aff9e0e26c78c6b94f6bd8fcba2afef9c4d41f
NusRAT-Lia <nusratlia424@gmail.com>
2022-12-27 02:24:31 +0600

    design done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { RegistrationForm } from './components';
+
+function App() {
+  return (
+    <div className="gradient-bg-header flex flex-col justify-center items-center">
+        <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/RegistrationFrom.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+
+const RegistrationFrom =()=>
+{
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    return(
+        <div className="form" >
+            <div className="col-1 flex flex-col justify-center items-center">
+              dcd
+            </div>
+           <div className="col-2">
+            ugfghj
+           </div>
+        </div>
+    );
+}
+export default RegistrationFrom;
